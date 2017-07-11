3023c19a8b3b88f55cfb0a2959f5dde314aaf7a0
wjdqls62 <wjdqls62@naver.com>
2017-07-11 15:19:57 +0900

    bring-up 4.0
    bring-up 4.0

--- app/src/main/java/com/jb/smartsetting/GPS_Utility/ProximityLocationService.java
+++ app/src/main/java/com/jb/smartsetting/GPS_Utility/ProximityLocationService.java
@@ -8,13 +8,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.location.LocationManager;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.jb.smartsetting.Common_Utility.ObjectReaderWriter;
-import com.jb.smartsetting.ILocationService;
 
 import java.util.ArrayList;
 
@@ -32,45 +30,75 @@ public class ProximityLocationService extends Service {
 
     private Intent intent;
     private IntentFilter intentFilter;
-    private PendingIntent pendingIntent;
+    private PendingIntent pIntent;
+    private ArrayList<PendingIntent> enabledPendingIntent;
 
     private String TAG = getClass().getName();
     private boolean isDebug = true;
 
     @Override
     public void onCreate() {
-        super.onCreate();
+
         context = getApplicationContext();
         mEnabledTargetLocation = new ArrayList<Stub_Location_Object>();
+        enabledPendingIntent = new ArrayList<PendingIntent>();
+
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         gpsReceiver = new GPS_Receiver();
 
         objectReaderWriter = new ObjectReaderWriter(getApplicationContext());
-        Log.d(TAG, "onCreate");
 
+        if (isDebug) {
+            Log.d(TAG, "onCreate");
+        }
+        super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // 서비스가 시작되면 활성화되어 있는 Location을 찾는다
+        if (isDebug) {
+            Log.d(TAG, "onStartCommand");
+        }
+
+
+
         mEnabledTargetLocation = objectReaderWriter.readObject();
-        for(int i=0; i<mEnabledTargetLocation.size(); i++){
-            if(mEnabledTargetLocation.get(i).isEnabled){
-                try{
+        intentFilter = new IntentFilter();
+
+        try {
+            for (int i = 0; i < mEnabledTargetLocation.size(); i++) {
+                if (mEnabledTargetLocation.get(i).isEnabled) {
                     this.intent = new Intent(String.valueOf(mEnabledTargetLocation.get(i).indentificationNumber));
-                    intentFilter = new IntentFilter(String.valueOf(mEnabledTargetLocation.get(i).indentificationNumber));
-                    pendingIntent = PendingIntent.getBroadcast(context, 1, this.intent, 0);
-                    registerReceiver(gpsReceiver, intentFilter);
+                    pIntent = PendingIntent.getBroadcast(context, i, this.intent, 0);
+
+                    enabledPendingIntent.add(pIntent);
+                    intentFilter.addAction(String.valueOf(mEnabledTargetLocation.get(i).indentificationNumber));
+
                     locationManager.addProximityAlert(
                             mEnabledTargetLocation.get(i).Latitude,
                             mEnabledTargetLocation.get(i).Longitude,
-                            100f,
+                            100,
                             -1,
-                            pendingIntent);
-                }catch (SecurityException e){
+                            pIntent);
+
+                    if (isDebug) {
+                        Log.d(TAG, "========================== addProximityAlert =============================");
+                        Log.d(TAG, "Enable Location Name : " + mEnabledTargetLocation.get(i).locationName);
+                        Log.d(TAG, "Lat : " + mEnabledTargetLocation.get(i).Latitude);
+                        Log.d(TAG, "Long : " + mEnabledTargetLocation.get(i).Longitude);
+                        Log.d(TAG, "Pending Intent Action : " + String.valueOf(mEnabledTargetLocation.get(i).indentificationNumber));
+                        Log.d(TAG, "==========================================================================");
+                    }
                 }
             }
+
+        } catch (SecurityException e) {
+
+        } finally {
+            registerReceiver(gpsReceiver, intentFilter);
         }
+
         return START_STICKY;
     }
 
@@ -92,28 +120,37 @@ public class ProximityLocationService extends Service {
 
     @Override
     public void onDestroy() {
+        if (isDebug) Log.d(TAG, "onDestroy");
+        unregisterReceiver(gpsReceiver);
         super.onDestroy();
     }
 
-    private class GPS_Receiver extends BroadcastReceiver{
+
+    public class GPS_Receiver extends BroadcastReceiver {
+        private String Current_Location = null;
 
         @Override
         public void onReceive(Context context, Intent intent) {
             boolean isEntering = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false);
-            for(int i=0; i<mEnabledTargetLocation.size(); i++){
-                if(String.valueOf(mEnabledTargetLocation.get(i).indentificationNumber)== intent.getAction()){
-                    if(isEntering){
-                        Toast.makeText(context, mEnabledTargetLocation.get(i).getLocationName()+"과 근접합니다.", Toast.LENGTH_SHORT).show();
-                        //sCallback.changeCustomSetting();
-                    }else{
-                        Toast.makeText(context, mEnabledTargetLocation.get(i).getLocationName()+"과 멀어졌습니다.", Toast.LENGTH_SHORT).show();
-                        //sCallback.restoreSetting();
+            if(isEntering){
+                for(int i=0; i<mEnabledTargetLocation.size(); i++){
+                    if(String.valueOf(mEnabledTargetLocation.get(i).indentificationNumber).equals(intent.getAction())){
+                        Toast.makeText(getApplicationContext(), mEnabledTargetLocation.get(i).getLocationName()+"에 접근", Toast.LENGTH_SHORT).show();
+                        Current_Location = mEnabledTargetLocation.get(i).getLocationName();
+                        break;
                     }
                 }
+            }else{
+                Toast.makeText(getApplicationContext(), Current_Location +"에 멀어짐", Toast.LENGTH_SHORT).show();
+                Current_Location = null;
             }
 
-
-            }
+            if(isDebug){
+            Log.d(TAG, "========================== GPS_Receiver.onReceive =============================");
+            Log.d(TAG, "isEntering : " + isEntering);
+            Log.d(TAG, "Intent.getAction : " + intent.getAction());
+            Log.d(TAG, "==============================================================================");
+        }
         }
     }
-
+}
